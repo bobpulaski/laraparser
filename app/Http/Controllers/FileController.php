82b63c5ba8cd9ad4dfa0aa4129c7309f0435202b
bobpulaski <82018695+bobpulaski@users.noreply.github.com>
2022-04-27 13:16:27 +0300

    27-04-2020-jod-BD-connect

--- app/Http/Controllers/FileController.php
+++ app/Http/Controllers/FileController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use Illuminate\Http\File;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Storage;
+use Illuminate\Support\Facades\DB;
 
 class FileController extends Controller
 {
@@ -13,6 +14,9 @@ class FileController extends Controller
         $filename = Storage::path('urls.txt');
         foreach(file($filename) as $line) {
             echo($line . '<br>');
+            DB::table('urls')->insert([
+                'url' => $line,
+            ]);
         }
     }
 }
