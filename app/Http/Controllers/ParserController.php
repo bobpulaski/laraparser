258a8fcd2e935b10142ab5c770535b0bbcf171fb
Bob Pulaski Home <mynameisempty@yandedex.ru>
2022-06-16 20:43:19 +0300

    16-06-22-home

--- app/Http/Controllers/ParserController.php
+++ app/Http/Controllers/ParserController.php
@@ -8,17 +8,41 @@ use Illuminate\Http\Request;
 
 class ParserController extends Controller
 {
+    public function get_string_between($string, $start, $end) {
+        $string = ' ' . $string;
+        $ini = strpos($string, $start);
+        if ($ini == 0) return '';
+        $ini += strlen($start);
+        $len = strpos($string, $end, $ini) - $ini;
+        return substr($string, $ini, $len);
+    }
+    
+    
+    
+    
     public function play ($id) {
         //dd($id);
 
         $urls = Url::where('chapter_id', $id)->pluck('url');
-        $rules = Rule::where('chapter_id', $id)->pluck('header_name');
+        $rules = Rule::where('chapter_id', $id)->get();
 
         foreach ($urls as $url)
         {
-            print_r($url . '<br>');
+            $content = file_get_contents ($url);
+            foreach ($rules as $rule)
+            {
+                $fullstring = $content;
+                print_r($url . '<br>');
+                $parsed = $this->get_string_between($fullstring, $rule->rule_left, $rule->rule_right);
+                print_r($parsed . '<br>');
+            }
+
+            //print_r($url . '<br>');
+
         }
 
 
     }
+
+    
 }
