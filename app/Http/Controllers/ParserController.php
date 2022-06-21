524dae524a97789eb1220ec58e10a8fa39db1bfd
bobpulaski <82018695+bobpulaski@users.noreply.github.com>
2022-06-21 15:36:37 +0300

    '21-06-22-job'

--- app/Http/Controllers/ParserController.php
+++ app/Http/Controllers/ParserController.php
@@ -30,70 +30,99 @@ class ParserController extends Controller
 
         $urls = Url::where('chapter_id', $id)->pluck('url');
         $rules = Rule::where('chapter_id', $id)->get();
-        $j = -1;
-        /*
+        $j = 0;
+
                 foreach ($urls as $url) {
                     $content = file_get_contents($url);
-
+                    $incoming = [];
                     foreach ($rules as $rule) {
-                        $j++;
+
                         $parsed = $this->get_string_between($content, $rule->rule_left, $rule->rule_right);
-                        $data[$j] = [
-                            'user_id' => Auth::id(),
+                        $incoming[] = [$rule->header_name => $parsed];
+
+                        /*$data[] = [
+                            'result' => $parsed,
+                            /*'user_id' => Auth::id(),
                             'chapter_id' => $rule->chapter_id,
                             'project_id' => $rule->project_id,
-                            'ext_header_name' => $rule->header_name,
-                            'ext_result' => $parsed,
-                        ];
+                            'ext_header_name' => '',
+                            'ext_result' => $rule->header_name .'##'. $parsed,*/
+                        //];
                     }
 
+                    $ext_results_array[] = ['jopa' => $incoming];
                 }
 
-                Result::where('chapter_id', $rule->chapter_id)->delete();
-                $result = Result::insert($data);
-                */
+                dd($ext_results_array);
+
+        return view('results')
+            /*->with('headers', $headers)*/
+            ->with('ext_results_array', $ext_results_array);
+
+                /*Result::where('chapter_id', $rule->chapter_id)->delete();
+                Result::insert($data);*/
 
 
-        $count_strings = DB::table('results')
+
+
+        /*$count_strings = DB::table('results')
             ->select('ext_header_name', 'chapter_id')
             ->where('chapter_id', $id)
             ->groupBy('ext_header_name', 'chapter_id')
-            ->count();
+            ->count();*/
 
-        $headers = DB::table('rules')
+        /*$headers = DB::table('rules')
             ->where('chapter_id', $id)
-            ->get('header_name');
+            ->get('header_name');*/
 
 
         //Получаем все заголовки из таблицы Rules
-        $headers = DB::table('rules')
+        /*$headers = DB::table('rules')
             ->where('chapter_id', $id)
-            ->get('header_name');
+            ->get('header_name');*/
 
 
-        //Получаем все результаты из таблицы Results
-        $results = DB::table('results')
+        //Получаем все заголовки из таблицы Results
+        /*$headers = DB::table('results')
+            ->select('ext_header_name')
             ->where('chapter_id', $id)
-            ->get();
+            ->groupBy('ext_header_name')
+            ->get();*/
+
+        /*$headers_count = count($headers);*/
+
+
+        //dd($headers_count);
+
+        /*          for ($i = 1; $i <= $headers_count; $i++) {
+
+                  }*/
+       /* $ext_results_array = DB::table('results')->get();*/
+
+        /*foreach ($headers as $header) {
+            //$ext_results_array[] = DB::table('results')->where('ext_header_name', $header->ext_header_name)->get('ext_result');
+            //print_r($header->ext_header_name . '</br>');
+            $ext_results_array[] = [$header->ext_header_name => DB::table('results')->where('ext_header_name', $header->ext_header_name)->get('ext_result')];
+        }*/
+
+        //dd($ext_results_array);
+        //dd($headers);
 
         //Проходим по каждому заголовку и делаем выборку результатов для каждого заголовка
-        $i = -1;
+        /*$i = -1;
         foreach ($headers as $header) {
-            $i++;
-            //dd($header->header_name);
-            //$ext_results_array[$i] = [$header->header_name => DB::table('results')->where('ext_header_name', $header->header_name)->find('ext_result')];
-            //$ext_results_array[$i] = [$header->header_name => DB::table('results')->where('ext_header_name', $header->header_name)->get()];
-        }
-
+            $i++;*/
+        //dd($header->header_name);
+        //$ext_results_array[$i] = [$header->header_name => DB::table('results')->where('ext_header_name', $header->header_name)->find('ext_result')];
+        //$ext_results_array[$i] = [$header->header_name => DB::table('results')->where('ext_header_name', $header->header_name)->get()];
 
         //$arr = ($ext_results_array);
         //dd($results);
         //var_dump($ext_results_array);
 
-        return view('results')
-            ->with('ext_results_array', $results);
-
+        /*return view('results')
+            ->with('headers', $headers)
+            ->with('ext_results_array', $ext_results_array);*/
 
     }
-
 }
--- resources/views/results.blade.php
+++ resources/views/results.blade.php
@@ -1,25 +1,19 @@
 <table>
     <thead>
     <tr>
-
-
-        {{--<pre><code>{{ json_encode($ext_results_array, JSON_PRETTY_PRINT) }}</code></pre>--}}
-
-        {{--@foreach($ext_results_array as $ext_headers)
-            <td>{{ $ext_headers->ext_header_name }}</td>
+        {{--@foreach($headers as $header)
+            <td>{{ $header->ext_header_name }}</td>
         @endforeach--}}
-
-        @php
-            $i = 0;
-        @endphp
-
-        @foreach($ext_results_array as $ext_headers)
-            <td>{{ $ext_headers->ext_header_name }}</td>
-            <td>{{ $ext_headers->ext_result }}</td>
-        @endforeach
-
-
-
     </tr>
     </thead>
+
+    <tbody>
+
+    @foreach($ext_results_array as $result)
+        <tr>
+            <td>{{ $result['jopa']['title'] }}</td>
+        </tr>
+    @endforeach
+    </tbody>
+
 </table>
