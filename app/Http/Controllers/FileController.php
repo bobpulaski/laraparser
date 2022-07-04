92885b99e0d53785d6ca7eead59d840fdd16b9f3
bobpulaski <82018695+bobpulaski@users.noreply.github.com>
2022-07-04 15:46:50 +0300

    '04-07-22-job'

--- app/Http/Controllers/FileController.php
+++ app/Http/Controllers/FileController.php
@@ -45,7 +45,60 @@ class FileController extends Controller
         }
 
         $filename = Str::random(20) . '_' . $project_id . '_' . $chapter_id . '.csv';
-        Storage::disk('local')->put($filename, 'Your content here');
+
+        //Формирование данных для записи в файл
+
+        //Получение заголовков
+        $headers = DB::table('results')->select('ext_header_name')
+            ->where('chapter_id', $chapter_id)
+            ->groupBy('ext_header_name')
+            ->orderByDesc('ext_header_name')
+            ->get();
+
+        //Формирование строки с заголовками
+        $csvHeaders = '';
+        foreach ($headers as $header) {
+            $csvHeaders .= $header->ext_header_name . ';';
+        }
+        $csvHeaders .= PHP_EOL;
+
+        //Получение данных
+        $datas = DB::table('results')->select('ext_result')
+            ->where('chapter_id', $chapter_id)
+            ->get();
+        //dd($datas[1]);
+
+        //Формирование строк с данными
+        $csvData = '';
+        $total = '';
+        $step = 0;
+        for ($i = 0; $i < count($datas); $i++) {
+
+            if ($step != count($headers)) {
+                $csvData .= $datas[$i]->ext_result . ';';
+                //print_r(' $step = ' . $step . '</br>');
+                //print_r($i . $csvData . '</br>');
+                $step++;
+            } else {
+                print_r(' Вошли в else $step = ' . $step . '</br>');
+                $csvData .= PHP_EOL;
+                $csvData .= $datas[$i--]->ext_result . ';';
+                print_r('ppp ' . $i);
+                $csvData .= $datas[$i + $step]->ext_result . ';' . PHP_EOL;
+                $total .= $csvData;
+                print_r('total = ' . $total . '</br>');
+                //print_r('total = ' . $total . '</br>');
+                $csvData = '';
+                $step = 0;
+            }
+        }
+
+        dd($total);
+
+        //////////////////////////////////////
+
+
+        Storage::disk('local')->put($filename, $csvAllData);
 
         //!!! Переменные приходят умножеными на 19
         //!!! Перед использованием в запросах необходимо их вернуть путем деления на 19
@@ -56,7 +109,7 @@ class FileController extends Controller
         /*        $getChapter->full_filename = $filename;
                 $getChapter->save();*/
 
-        dd(Storage::fileExists($filename));
+        //dd(Storage::fileExists($filename));
 
         //!!! Переменные приходят умножеными на 19
         //!!! Перед использованием в запросах необходимо их вернуть путем деления на 19
@@ -71,7 +124,6 @@ class FileController extends Controller
             ->first('full_filename');
 
         $path = storage_path('app/' . $getCurrentFullFileName->full_filename);
-        //dd($path);
 
         return response()->download($path);
     }
